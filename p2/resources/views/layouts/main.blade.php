adfba710ea5a4385f711ea1bae901963bcbe1a7c
Paula Leal <paula61395@gmail.com>
2022-03-15 02:04:19 +0000

    added Boostrap styling

--- p2/resources/views/layouts/main.blade.php
+++ p2/resources/views/layouts/main.blade.php
@@ -4,23 +4,25 @@
 <head>
     <title>Hobbyist</title>
     <meta charset='utf-8'>
-    <link href='/css/styles.css' type='text/css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href='/public/css/styles.css' type='text/css' rel='stylesheet'>
 
     @yield('head')
 </head>
 
-<body>
+<body class="min-vh-100 bg-light">
 
     <header>
-        Discover a new hobby today.
+        <h1 class="text-center">Hobbyist<h1>
     </header>
 
-    <section>
+    <section class="d-flex flex-column justify-content-center align-items-center py-5">
         @yield('content')
     </section>
 
-    <footer>
-        Made with &#9829; by Paula LF 2022
+    <footer class="text-center py-4 mt-auto">
+        <p class="my-1 fs-5">Made with ❤️ by Paula LF 2022</p>
     </footer>
 
 </body>
--- p2/resources/views/pages/home.blade.php
+++ p2/resources/views/pages/home.blade.php
@@ -2,42 +2,51 @@
 
 @section('content')
 
-    <form method='GET' action='/search'>
-        <h2>Search for a hobby under a specific category.</h2>
+    <form method='GET' action='/search' class="col-lg-6 align-items-start">
+        <div class="text-center">
+            <h3>Search for a new hobby under specific category.</h3>
+        </div>
 
         <fieldset>
+            <div class="row my-5">
+                <label for="categories" class="form-label col-form-label">Choose a Category:</label>
+                <select name="categories" id="categories" class="form-select">
+                    <option selected disabled>Select a Category</option>
+                    <option value=" physical" {{ old('categories') == 'physical' ? 'selected' : '' }}>Physical
+                    </option>
+                    <option value="creative" {{ old('categories') == 'creative' ? 'selected' : '' }}>Creative
+                    </option>
+                    <option value="mental" {{ old('categories') == 'mental' ? 'selected' : '' }}>Mental</option>
+                    <option value="food" {{ old('categories') == 'food' ? 'selected' : '' }}>Food</option>
+                    <option value="collecting" {{ old('categories') == 'collecting' ? 'selected' : '' }}>Collecting
+                    </option>
+                    <option value="games" {{ old('categories') == 'games' ? 'selected' : '' }}>Games/Puzzles</option>
+                </select>
+            </div>
 
-            <label for="categories">Choose a Category:</label>
+            <div class="form-group row my-5">
+                <label for="sugesstionNumber" class="form-label col-form-label">
+                    Enter the amount of suggestions you want:
+                    <input type="text" class="form-control form-control-sm"" name=" suggestionNumber"
+                        value='{{ old('suggestionNumber') }}'>
+                </label>
+            </div>
 
-            <select name="categories" id="categories">
-                <option selected disabled>Select a Category</option>
-                <option value="physical" {{ old('categories') == 'physical' ? 'selected' : '' }}>Physical
-                </option>
-                <option value="creative" {{ old('categories') == 'creative' ? 'selected' : '' }}>Creative
-                </option>
-                <option value="mental" {{ old('categories') == 'mental' ? 'selected' : '' }}>Mental</option>
-                <option value="food" {{ old('categories') == 'food' ? 'selected' : '' }}>Food</option>
-                <option value="collecting" {{ old('categories') == 'collecting' ? 'selected' : '' }}>Collecting
-                </option>
-                <option value="games" {{ old('categories') == 'games' ? 'selected' : '' }}>Games/Puzzles</option>
-            </select>
-
-            <label for="sugesstionNumber">
-                Enter the amount of suggestions you want:
-                <input type="text" name="suggestionNumber" value='{{ old('suggestionNumber') }}'>
-            </label>
-
-            <label for="inspirationalQuote">
-                Want a motivational quote? Yes!
-                <input type="checkbox" name="inspirationalQuote" value="yes" id='inspirationalQuote'
-                    {{ old('inspirationalQuote') == 'yes' ? 'checked' : '' }}>
-            </label>
+            <div class="form-check row my-5">
+                <label for="inspirationalQuote form-check-label col-form-label">
+                    Want a motivational quote? Yes!
+                    <input type="checkbox" class="form-check-input" name="inspirationalQuote" value="yes"
+                        id='inspirationalQuote' {{ old('inspirationalQuote') == 'yes' ? 'checked' : '' }}>
+                </label>
+            </div>
         </fieldset>
 
-        <button type='submit' class='btn btn-primary'>Enter</button>
+        <div class="d-grid gap-2 col-6 mx-auto">
+            <button type="submit" class="btn btn-primary p-2" type="button">Submit</button>
+        </div>
 
         @if (count($errors) > 0)
-            <ul class='alert alert-danger'>
+            <ul class='alert alert-danger my-2 p5'>
                 @foreach ($errors->all() as $error)
                     <li>{{ $error }}</li>
                 @endforeach
@@ -51,12 +60,12 @@
                 No results found.
             </div>
         @else
-            <div class='results alert alert-primary'>
+            <div class='results alert alert-primary m-3 p1 fs-4'>
 
                 {{ count($searchResults) }}
                 {{ Str::plural('Result', count($searchResults)) }}:
 
-                <ul class='clean-list'>
+                <ul class='p5 fs-4'>
                     @foreach ($searchResults as $result)
                         <li>{{ $result }}</li>
                     @endforeach
@@ -66,8 +75,10 @@
     @endif
 
     @if (!is_null($inspirationalQuote))
-        <div>{{ $inspirationalQuote['text'] }}</div>
-        <div>{{ $inspirationalQuote['author'] }}</div>
+        <div class="results alert alert-primary text-center fs-4">
+            <div>{{ $inspirationalQuote['text'] }}</div>
+            <div> ~ {{ $inspirationalQuote['author'] }}</div>
+        </div>
     @endif
 
 @endsection
