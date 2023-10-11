94784b7b8154b9a70790a3106d5f8177a06205cc
Paula Leal <paula61395@gmail.com>
2023-10-11 19:05:01 +0000

    fixed appserviceprovider

--- p3/app/Providers/AppServiceProvider.php
+++ p3/app/Providers/AppServiceProvider.php
@@ -24,11 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-<<<<<<< HEAD
-        if (env('APP_ENV') == 'production') {
-=======
+
          if (env('APP_ENV') == 'production') {
->>>>>>> 376b489b98362dfc93e5218c5db595666170e0a3
+
             $url->forceScheme('https');
         }
     }
